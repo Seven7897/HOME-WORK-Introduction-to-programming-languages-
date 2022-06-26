6c580a16ee5a8886040909174f98d88f13f8053b
Anatoly Finchenko <finchenko777@gmail.com>
2022-06-26 18:50:00 +0300

    *Первое_задание*

---
+++ homework001/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая на вход принимает два числа и выдаёт, 
+какое число большее, а какое меньшее.
+*/
+
+Console.WriteLine("Введите первое число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число : ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a ;
+int min = b ;
+
+if (a > b ) 
+  {
+    Console.WriteLine("большее число : " + max);
+    Console.WriteLine("Меньшее число : " + min);
+  }
+  else 
+  {
+  max = b ;
+  min = a ;
+  Console.WriteLine("большее число : " + max);
+  Console.WriteLine("Меньшее число : " + min);
+  }
+
